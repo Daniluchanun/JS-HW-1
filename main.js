13ad8f6276a807b850b0fe297750b8db50f1e318
Daniluchanun <Danylm30@gmail.com>
2024-08-07 17:04:16 +0300

    asd

---
+++ main.js
@@ -0,0 +1,62 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.Вивести кожну змінну за допомогою: console.log
+
+
+let word1 = 'hello';
+console.log(word1);
+
+let word2 = 'owu';
+console.log(word2);
+
+let word3 = 'com';
+console.log(word3);
+
+let word4 = 'ua';
+console.log(word4);
+
+let num1 = 1;
+console.log(num1);
+
+let num2 = 10;
+console.log(num2);
+
+let num3 = -999;
+console.log(num3);
+
+let num4 = 123;
+console.log(num4);
+
+const PI = 3.14;
+console.log(PI);
+
+let num5 = 2.7;
+console.log(num5);
+
+let num6 = 16;
+console.log(num6);
+
+let bool1 = true;
+console.log(bool1);
+
+let bool2 = false;
+console.log(bool2);
+// console.log(!!'1')
+// console.log(!!'')
+
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+let firstName = 'Daniil';
+let middleName = 'Andrew';
+let lastName = 'Magro';
+console.log(firstName + ' ' + middleName + ' ' + lastName);
+// console.log(`${firstName} ${middleName} ${lastName}`);
+
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
